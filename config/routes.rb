e9ee96d4876ad2f2d3f7d54498137ff3c07d225e
makiwakurin <takahiro1608@gmail.com>
2020-12-11 10:40:29 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+=======
+  devise_for :users
+  root to: 'campsites#index'
+  resources :campsites, only: [:index]
+
+>>>>>>> Stashed changes
 end
