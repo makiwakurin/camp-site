e648806e61c9701b4b839d17d1b408b19f5b096b
makiwakurin <takahiro1608@gmail.com>
2020-12-17 12:05:25 +0900

    再度修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< Updated upstream
   root to: "campsites#index" 
-=======
-<<<<<<< Updated upstream
-  root to: 'campsites#index'
   resources :campsites, only: [:index]
-=======
-  root to: "campsites#index" 
-  
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 end
