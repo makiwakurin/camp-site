7f9571c95eb7e036b424c719f972e7a450866eba
makiwakurin <takahiro1608@gmail.com>
2020-12-17 11:50:54 +0900

    indexアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+<<<<<<< Updated upstream
   root to: 'campsites#index'
   resources :campsites, only: [:index]
+=======
+  resources :campsites, only: [:index]
+  root to: "campsites#index" 
+>>>>>>> Stashed changes
 end
