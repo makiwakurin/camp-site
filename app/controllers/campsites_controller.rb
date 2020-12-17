de013637e14b0fad7f17fc7ce149ad10d80957ca
makiwakurin <takahiro1608@gmail.com>
2020-12-17 12:32:01 +0900

    new,createアクション処理記述

--- app/controllers/campsites_controller.rb
+++ app/controllers/campsites_controller.rb
@@ -3,11 +3,24 @@ class CampsitesController < ApplicationController
   def index
   end
 
-　def new
+  def new
     @campsite = Campsite.new
   end
 
-  
+  def create
+    @campsite = Campsite.new(campsite_params)
+    if @campsite.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private 
+
+  def campsite_params
+    params.require(:campsite).permit(:name, :text)
+  end
 
 end
+
