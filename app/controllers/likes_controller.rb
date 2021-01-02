3ea943161de103e74402bb336498c87cbfd67d29
makiwakurin <takahiro1608@gmail.com>
2021-01-02 23:45:50 +0900

    redirect_to 削除

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,10 @@ class LikesController < ApplicationController
 
   def create
     Like.create(user_id: current_user.id, campsite_id: params[:id])
-    redirect_to root_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, campsite_id: params[:id]).destroy
-    redirect_to root_path
   end
 
 end
