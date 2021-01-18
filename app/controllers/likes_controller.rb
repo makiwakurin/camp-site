c64ebdc0bc0daefb85c8d7e373d70f63f4e0a009
makiwakurin <takahiro1608@gmail.com>
2021-01-18 22:41:56 +0900

    post_params 定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,7 @@
 class LikesController < ApplicationController
 
+  before_action :post_params
+
   def create
     Like.create(user_id: current_user.id, campsite_id: params[:id])
     redirect_to campsite_path
@@ -8,6 +10,12 @@ class LikesController < ApplicationController
   def destroy
     Like.find_by(user_id: current_user.id, campsite_id: params[:id]).destroy
     redirect_to campsite_path
+  end
+
+  private
+
+  def post_params
+    @campsite = Campsite.find(params[:id])
   end
 
 end
